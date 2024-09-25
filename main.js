1cbde5aee6ff60d3a8fe955ec9ac65bd4c8f3e5d
Ronald Pasion <52368740+RonaldPasion@users.noreply.github.com>
2024-09-25 15:24:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ userName ? console.log(`Hello, ${userName}!`): console.log('Hello!');
 const userQuestion = 'Is it going to rain today?';
 
 userName ? console.log(`${userName} asked, ${userQuestion}`): console.log(`You asked, ${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random() * 8);
